6a85f6b7f8b861fe6fa1c50e7dd13f3541fa06c9
Steven Hepting <shepting@gmail.com>
2016-02-17 16:36:46 -0800

    Add initial project files.

---
+++ CloudButton/CloudButton/CloudButton.h
@@ -0,0 +1,19 @@
+//
+//  CloudButton.h
+//  CloudButton
+//
+//  Created by Steven Hepting on 2/17/16.
+//  Copyright © 2016 Hepting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CloudButton.
+FOUNDATION_EXPORT double CloudButtonVersionNumber;
+
+//! Project version string for CloudButton.
+FOUNDATION_EXPORT const unsigned char CloudButtonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CloudButton/PublicHeader.h>
+
+
